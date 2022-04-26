0b89ee3ced5e36ebefb93086b473404425d97661
LD Samson <l.d.samson@gmail.com>
2022-04-26 17:13:58 +0200

    added example to plot_p_histogram. improved description.

--- R/plots.R
+++ R/plots.R
@@ -1,14 +1,26 @@
 #' Function to plot P histogram
 #'
+#' If no associations are found, by definition the pvalues should come from
+#' a uniform distribution. This can be checked with the function below.
+#'
 #' @param data dataframe to use
 #' @param p_value character string. name of column containing p values
 #' @param title.to.plot title of the plot to produce
-#' @param gamma.hat gamma hat to use
+#' @param gamma.hat
+#' gamma hat to use. Standard set to 1. In future versions, this value could
+#' be changed in order to get more accurate detection of associations.
 #' @importFrom rlang .data
 #'
 #' @return histogram
 #' @export
 #'
+#' @examples
+#' # Create random P values, possible result of an association study
+#' pvals_data <- data.frame(
+#' "p.value" = c(runif(1000, 0, .99999), runif(250, 1.0e-11, 1.0e-8))
+#' )
+#' plot_p_histogram(pvals_data)
+
 plot_p_histogram <- function(
     data,
     p_value = "p.value",
